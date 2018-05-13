0353b0fa73f96c63177f9ed3a7c14a0cf20deff0
applesaucee <tamvn@uw.edu>
2018-05-13 13:42:37 -0700

    Fixed presale calculator bug

--- js/counter.js
+++ js/counter.js
@@ -69,6 +69,9 @@ function calcPresales() {
             $('#seats').text(seatsRemaining);
             $('#refund').removeAttr("disabled");
             $('#presales').attr("disabled", "disabled");
+            if (seatsRemaining === 0) {
+                $('#sell').attr("disabled", "disabled");
+            }
         }
     }
 }
